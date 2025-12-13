01f78271a29edeae92dd55bbf42a2236a779e486
Rama <syahrul.rama33@gmail.com>
2025-12-14 00:27:44 +0700

    stage 1 done

--- trial1.js
+++ trial1.js
@@ -5,13 +5,22 @@
 4. Selain itu, tampilkan angkanya seperti biasa
 */
 
-let counter = 0
-for (let x = 0; x <= 30; x++) {
-    if (counter === 4) {
-        console.log("empat");
-        counter = 0
-    } else if (x) {
-        console.log(x);
-    }
-    counter++
+let counter4 = 0;
+let counter6 = 0;
+for (let x = 1; x <= 30; x++) {
+	counter4++;
+	counter6++;
+	if (counter4 === 4 && counter6 === 6) {
+		console.log('empat-enam');
+		counter6 = 0;
+		counter4 = 0;
+	} else if (counter6 === 6) {
+		console.log('enam');
+		counter6 = 0;
+	} else if (counter4 === 4) {
+		console.log('empat');
+		counter4 = 0;
+	} else {
+		console.log(x);
+	}
 }
