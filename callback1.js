b8739e106678f0ae1418c547908e65c5b9157ece
Rama <syahrul.rama33@gmail.com>
2026-01-17 11:57:08 +0700

    callback

--- callback1.js
+++ callback1.js
@@ -42,6 +42,55 @@
 // ==============================================
 // --------------- L I N E 17 -------------------
 // ==============================================
+const digimons = [
+	{
+		name: 'Agumon',
+		level: 5,
+		hp: [30, 20, 25],
+	},
+	{
+		name: 'Gabumon',
+		level: 4,
+		hp: [20, 15],
+	},
+	{
+		name: 'Patamon',
+		level: 3,
+		hp: [],
+	},
+];
+function processDigimons(data, callback) {
+	let result = [];
+	for (let i = 0; i < data.length; i++) {
+		const digimon = data[i];
+		if (digimon.hp.length === 0) continue;
+		result.push(callback(digimon));
+	}
+	return result;
+}
+
+function digiStats(result) {
+	let status = result['hp'];
+	let total = 0;
+	for (let i = 0; i < status.length; i++) {
+		const stat = status[i];
+		if (stat.length === 0) {
+			continue;
+		}
+		total += stat;
+	}
+
+	let avg = Math.floor(total / status.length);
+
+	return {
+		name: result.name,
+		level: result.level,
+		averageHp: avg,
+	};
+}
+
+let show = processDigimons(digimons, digiStats);
+console.log(show);
 
 // ==============================================
 // --------------- L I N E 18 -------------------
